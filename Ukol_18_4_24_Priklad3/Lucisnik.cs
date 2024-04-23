d5d4798084c78e89c310b10a9cb289fb661b863a
Zuzik91 <zuzana.smidova91@seznam.cz>
2024-04-23 09:38:06 +0200

    Jiz hotovy priklad

---
+++ Ukol_18_4_24_Priklad3/Lucisnik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukol_18_4_24_Priklad3
+{
+    /* Vytvořte třídu Lucistnik, ktera bude mit vlastnost PocetSipu a metodu Vystrel. PocetSipu muze byt nastaven treba na 10.
+    Pokud bude mit dost sipu, napise metoda Vystrel na konzoli text: Vzdy se strefim primo do prostred!
+    Metoda Vystrel uvnitr s kazdym vystrelem snizi pocet sipu.
+    Pokud bude pocet 0, metoda Vystrel vypise "Nemam sipy".*/
+    internal class Lucisnik
+    {
+        public int PocetSipu { get; set; }
+
+        public Lucisnik(int pocetSipu)
+        {
+            PocetSipu = pocetSipu;
+        }
+
+        public void Vystrel()
+        {
+            if (PocetSipu > 0)
+            {
+                PocetSipu--;
+                Console.WriteLine("Vzdy se strefim primo do prostred!");
+            }
+            else
+            {
+                Console.WriteLine("Nemam sipy");
+            }
+        }
+    }
+}
